4e4acfa3e82aa3a4e863fe7908e2d8b0fbe0c070
Leman FENG <flm8620@gmail.com>
2017-11-20 18:16:17 +0100

    minor changes to test LRMC algo

--- Low_Rank_Matrix.py
+++ Low_Rank_Matrix.py
@@ -1,7 +1,9 @@
 from tools import *
 
+np.random.seed(123456)
+
 # Creates a matrix whose columns are affinely dependent 
-X0 = np.random.randint(10, size=5)
+X0 = np.random.randint(10,20, size=5)  # generate in range [10,20], avoiding zero to distinguish easily later
 X = []
 for i in range(10):
     X.append(list(X0 + np.random.randint(-10,10)))
@@ -11,16 +13,24 @@ print("Initial matrix : ")
 print(np.around(X,decimals=2))
     
 # Choose observed entries at random
-W = np.random.randint(2, size=X.shape)
+gamma = 0.3  # ratio of missing entries
+indices = np.random.choice(X.size, int(gamma*X.size), replace=False)  # choose randomly indices to mask
+W = np.ones(X.shape)
+for i in indices:
+    W.itemset(i, 0)  # flatten indexing
 
 print("Observed entries : ")
 print(np.around(X*W,decimals=2))
 
 tau = 1000
 beta = 0.01
-epsilon = 0.01
+epsilon = 0.001
 
 A = LRMC(X, W, tau, beta, epsilon)
 
+np.set_printoptions(precision=2)
 print("Recovered matrix : ")
 print(np.around(A,decimals=2))
+
+print("Error matrix : ")
+print(np.around(A-X, decimals=2))
